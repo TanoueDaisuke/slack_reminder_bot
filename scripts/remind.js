5e31de73cb9007f71d56b10cf7cbf9b1d9c73171
TanoueDaisuke <tano.dai.0711@icloud.com>
2019-11-20 12:43:27 +0900

    リマインド設定まで

---
+++ scripts/remind.js
@@ -0,0 +1,9 @@
+const cronJob = import('cron').CronJob
+
+module.exports = robot　=> {
+  // 設定方法: https://00m.in/RhXkB
+  return newCronJob = new cronJob('00 45 12 20 11 *', function() {
+    const envelope = {room: "#general"}
+    return robot.send(envelope, "Hungry...!!!");
+  });
+};
