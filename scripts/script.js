80554bbd705d994b5d90a969d1adc6a60313a9c7
TanoueDaisuke <tano.dai.0711@icloud.com>
2019-11-20 15:03:11 +0900

    ほぼ完成

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,27 @@
 module.exports = robot => {
-  // 大文字小文字を区別せずhelloがマッチすれば、"Hello!"を返す
-  // https://00m.in/k3GXk //その他の設定
-  robot.hear(/hello/i, res => {    
-    // console.log(robot);
-    console.log(res.message.text);
-    
-    res.send(res.message.text)
+  const formatTwoDigits = num => num < 10 ? `0${num}` : num
+
+  const setCronJob = (date, res) => {
+    // todo: dateの処理
+    const minute = formatTwoDigits(date.getMinutes() + 2)
+    const hour = formatTwoDigits(date.getHours())
+    const tomorrowDate = formatTwoDigits(date.getDate())
+    const month = date.getMonth() // monthは２桁でなくてもいい
+
+    const newCronJob = new cronJob(`00 ${minute} ${hour} ${tomorrowDate} ${month} *`, function() {
+      const envelope = {room: "#general"}
+      return robot.send(envelope, `${res.message.text}`);
+    });
+    return newCronJob
+  }
+
+  robot.hear(/./i, res => {   
+    res.send("了解！リマインド設定します( ˘ω˘ )")
+    // res.send(res.message.text)
+    const date = new Date()
+    const newCronJob = setCronJob(date, res)
+
+    newCronJob.start()
   })
 }
+
