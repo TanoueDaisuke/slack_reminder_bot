7f75eb0552fa96620e5d1cdced9704f6d48e429e
TanoueDaisuke <tano.dai.0711@icloud.com>
2019-11-20 15:29:59 +0900

    完成

--- scripts/script.js
+++ scripts/script.js
@@ -6,16 +6,16 @@ module.exports = robot => {
 
   const setCronJob = (date, res) => {
     // dateの処理
-    const minute = formatTwoDigits(date.getMinutes() + 2)
+    const minute = formatTwoDigits(date.getMinutes())
     const hour = formatTwoDigits(date.getHours())
-    const tomorrowDate = formatTwoDigits(date.getDate())
+    const tomorrowDate = formatTwoDigits(date.getDate() + 1)
     const month = date.getMonth() // monthは２桁でなくてもいい
 
     const newCronJob = new cronJob(`00 ${minute} ${hour} ${tomorrowDate} ${month} *`, function() {
       const envelope = {room: "#general"}
-      return robot.send(envelope, `<!channel>${res.message.text}`) //res.message.textで送られたメッセージ取得
+      return robot.send(envelope, `<!channel>\n${res.message.text}`) //res.message.textで送られたメッセージ取得
     })
-    
+
     return newCronJob
   }
 
