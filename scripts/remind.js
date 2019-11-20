f28f80298eb6ac80fae54a83a06fab13b6f7119b
TanoueDaisuke <tano.dai.0711@icloud.com>
2019-11-20 13:48:23 +0900

    リマインド再設定

--- scripts/remind.js
+++ scripts/remind.js
@@ -2,7 +2,7 @@ const cronJob = require('cron').CronJob;
 
 module.exports = robot　=> {
   // 設定方法: https://00m.in/RhXkB
-  return newCronJob = new cronJob('00 55 12 20 11 *', function() {
+  return newCronJob = new cronJob('00 50 13 20 11 *', function() {
     const envelope = {room: "#general"}
     return robot.send(envelope, "Hungry...!!!");
   });
